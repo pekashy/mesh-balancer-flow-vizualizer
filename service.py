10bc333c150974af6352a57d72386aa26eb5c441
Egor Zyazev <zyazev.ea@phystech.edu>
2022-01-06 22:21:45 +0300

    Configure logging

--- service.py
+++ service.py
@@ -1,8 +1,10 @@
 from flask import Flask
 import os
+import logging
 
 app = Flask(__name__)
 healthy = True
+log = logging.getLogger('werkzeug')
 
 
 @app.route('/')
@@ -10,6 +12,16 @@ def hello():
     global healthy
     if healthy:
         return f"Hello from {os.environ['HOST']}!\n"
+    else:
+        return "Unhealthy", 503
+
+
+@app.route('/bench')
+def bench():
+    global healthy
+    log.warning('Hello World!')
+    if healthy:
+        return f"Bench from {os.environ['HOST']}!\n"
     else:
         return "Unhealthy", 503
 
@@ -29,4 +41,10 @@ def unhealthy():
 
 
 if __name__ == "__main__":
+    formatter = logging.Formatter(fmt="%(asctime)s.%(msecs)03d %(levelname)s %(module)s: %(message)s",
+                                  datefmt="%H:%M:%S")
+    log.setLevel(logging.WARNING)
+    file_handler = logging.FileHandler(f"/logs/{os.environ['HOST']}.log")
+    file_handler.setFormatter(formatter)
+    log.addHandler(file_handler)
     app.run(host='0.0.0.0', port=8000, debug=False)
